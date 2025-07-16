c9cb92592de3ede05a353a34a353894451abcea1
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-07-16 13:33:00 +0700

    refactor: user base schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,18 +1,20 @@
 import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
 import { users } from "~/database/schema/users";
-import type z from "zod";
 
 const baseSchema = createInsertSchema(users, {
-  id: schema => schema.id.readonly().describe("Unique identifier of the user"),
-  name: schema => schema.name.describe("Display name of the user"),
-  email: schema => schema.email.describe("Email address of the user"),
-  password: schema => schema.password.describe("Encrypted password of the user"),
-  createdAt: schema => schema.createdAt.readonly().describe("Creation date of the user as an ISO 8601 date string"),
-  updatedAt: schema => schema.updatedAt.readonly().describe("Modification date of the user as an ISO 8601 date string"),
+  id: schema => schema.readonly().describe("Unique identifier of the user"),
+  name: schema => schema.describe("Display name of the user"),
+  email: schema => schema.describe("Email address of the user"),
+  password: schema => schema.describe("Encrypted password of the user"),
+  createdAt: schema => z.iso.datetime().readonly().describe("Creation date of the user as an ISO 8601 date string"),
+  updatedAt: schema => z.iso.datetime().readonly().describe("Modification date of the user as an ISO 8601 date string"),
 });
 
 export const UserDTO = baseSchema.required()
   .describe("Represents a user definition");
+
+export type TypeOfUserDTO = z.infer<typeof UserDTO>;
 
 export const NewUserDTO = baseSchema.omit({
   id: true,
@@ -20,14 +22,9 @@ export const NewUserDTO = baseSchema.omit({
   updatedAt: true,
 }).describe("Data Transfer Object for creating a new user");
 
+export type TypeOfNewUserDTO = z.infer<typeof NewUserDTO>;
+
 export const UserPatchDTO = NewUserDTO.partial().omit({
 }).describe("Data Transfer Object for updating an existing user");
 
-export function testUserData() {
-  return {
-    name: "unknown",
-    email: "john.doe@example.com",
-    password: "123",
-    // add the required fields here
-  } satisfies z.infer<typeof NewUserDTO>;
-}
+export type TypeOfUserPatchDTO = z.infer<typeof UserPatchDTO>;
