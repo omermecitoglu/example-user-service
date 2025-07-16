1ac34be5512ef358c0051fd6852b1ce9035cdf92
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-07-16 13:10:42 +0700

    refactor: database migrate

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -1,7 +1,33 @@
 import getPackageMetadata from "@omer-x/package-metadata";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
-import { Client } from "pg";
+import { Client, type ClientConfig } from "pg";
+
+async function createDatabase(
+  creds: Pick<ClientConfig, "host" | "port" | "user" | "password" | "database">,
+  attempt = 1,
+): Promise<void> {
+  const creator = new Client({ ...creds, database: "postgres" });
+  try {
+    await creator.connect();
+    try {
+      await creator.query(`CREATE DATABASE "${creds.database}"`);
+    } catch {
+      // do nothing
+    } finally {
+      await creator.end();
+    }
+  } catch {
+    await creator.end();
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    // eslint-disable-next-line no-console
+    console.log("\x1b[33m%s\x1b[0m", "Retrying to create database...");
+    if (attempt > 500) {
+      throw new Error("Failed to create database after multiple attempts");
+    }
+    await createDatabase(creds, attempt + 1);
+  }
+}
 
 async function migrateAppDatabase() {
   const packageMetadata = getPackageMetadata();
@@ -14,25 +40,8 @@ async function migrateAppDatabase() {
     database: process.env.POSTGRES_DATABASE || packageMetadata.moduleName,
   };
 
-  const connect = async () => {
-    const creator = new Client({ ...dbCredentials, database: "postgres" });
-    try {
-      await creator.connect();
-      try {
-        await creator.query(`CREATE DATABASE "${dbCredentials.database}"`);
-      } catch {
-        // do nothing
-      } finally {
-        await creator.end();
-      }
-    } catch {
-      await creator.end();
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      await connect();
-    }
-  };
   if (dbCredentials.database === "verceldb") return;
-  await connect();
+  await createDatabase(dbCredentials);
 
   const client = new Client(dbCredentials);
   await client.connect();
